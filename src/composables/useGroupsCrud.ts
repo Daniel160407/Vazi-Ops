c18de60a03d7b1b0d2ab8b026596528789de2976
Daniel160407 <abulashvilidani@hotmail.com>
2026-03-01 19:46:49 +0400

    feat: ability to add and delete groups

--- src/composables/useGroupsCrud.ts
+++ src/composables/useGroupsCrud.ts
@@ -10,16 +10,32 @@ import { db } from "../../firebase";
 import { GROUPS_DB } from "../composables/constants";
 import type { Group } from "../type/interfaces";
 import { useToast } from "primevue";
+import { useGlobalStore } from "../stores/GlobalStore";
+import { storeToRefs } from "pinia";
 
 export function useGroupsCrud() {
   const toast = useToast();
+  const { groups } = storeToRefs(useGlobalStore());
 
-  const groups = ref<Group[]>([]);
   const loading = ref(false);
 
   const addGroup = async (group: Omit<Group, "id">) => {
     loading.value = true;
     try {
+      const isCottageOccupied = groups.value.some(
+        (g) => g.cottage_num === group.cottage_num
+      );
+
+      if (isCottageOccupied) {
+        toast.add({
+          severity: "error",
+          summary: "კოტეჯი დაკავებულია",
+          detail: `მე-${group.cottage_num} კოტეჯი უკვე დაკავებულია!`,
+          life: 3000,
+        });
+        return;
+      }
+
       const docRef = await addDoc(collection(db, GROUPS_DB), group);
       toast.add({
         severity: "success",
@@ -44,13 +60,27 @@ export function useGroupsCrud() {
     if (!group.id) return;
     loading.value = true;
     try {
+      const isCottageOccupied = groups.value.some(
+        (g) => g.cottage_num === group.cottage_num && g.id !== group.id
+      );
+
+      if (isCottageOccupied) {
+        toast.add({
+          severity: "error",
+          summary: "კოტეჯი დაკავებულია",
+          detail: `მე-${group.cottage_num} კოტეჯი უკვე დაკავებულია სხვა ჯგუფის მიერ!`,
+          life: 3000,
+        });
+        return;
+      }
+
       const groupRef = doc(db, GROUPS_DB, group.id);
       const { id, ...groupData } = group;
       await updateDoc(groupRef, groupData);
 
       toast.add({
         severity: "success",
-        summary: "განახლებული ჯგუფი შენახულია",
+        summary: "ჯგუფი განახლებულია",
         life: 3000,
       });
     } catch (err) {
@@ -89,7 +119,6 @@ export function useGroupsCrud() {
   };
 
   return {
-    groups,
     loading,
     addGroup,
     updateGroup,
