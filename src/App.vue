78dc931565c66effe31a2a7e5eebe071d38c4f8d
Daniel160407 <abulashvilidani@hotmail.com>
2026-07-03 00:22:26 +0400

    feat: implement mobile bottom navigation and extended menu sheet components

--- src/App.vue
+++ src/App.vue
@@ -1,209 +1,37 @@
 <script setup lang="ts">
-import { ConfirmDialog, Menubar, Toast } from "primevue";
-import { useRouter } from "vue-router";
-import {
-  ADMIN_GROUPS_ROUTE,
-  ADMIN_CLUBS_ROUTE,
-  ADMIN_CLUB_BOOKINGS_ROUTE,
-  CLUBS_ROUTE,
-  ADMIN_ICON,
-  ADMIN_LABEL,
-  ADMIN_CLUBS_LABEL,
-  ADMIN_CLUB_BOOKINGS_LABEL,
-  GROUPS_ICON,
-  GROUPS_LABEL,
-  GROUPS_ROUTE,
-  DAY_SCHEDULE_LABEL,
-  CLUBS_ICON,
-  DAY_SCHEDULE_ICON,
-  DAY_SCHEDULE_ROUTE,
-  ADMIN_DAY_SCHEDULE_LABEL,
-  ADMIN_DAY_SCHEDULE_ROUTE,
-  EVENING_SCHEDULE_LABEL,
-  EVENING_SCHEDULE_ICON,
-  EVENING_SCHEDULE_ROUTE,
-  ADMIN_EVENING_SCHEDULE_LABEL,
-  ADMIN_EVENING_SCHEDULE_ROUTE,
-  CLUBS_LABEL,
-  EVENTS_LABEL,
-  EVENTS_ICON,
-  EVENTS_ROUTE,
-  ADMIN_EVENTS_LABEL,
-  ADMIN_EVENTS_ROUTE,
-  CLUB_BOOKINGS_ICON,
-  GOLDEN_VERSES_ROUTE,
-  GOLDEN_VERSES_ICON,
-  GOLDEN_VERSES_LABEL,
-  ADMIN_GOLDEN_VERSES_ROUTE,
-  ANNOUNCEMENTS_LABEL,
-  ANNOUNCEMENTS_ROUTE,
-  ANNOUNCEMENTS_ICON,
-  ADMIN_ANNOUNCEMENTS_LABEL,
-  ADMIN_ANNOUNCEMENTS_ROUTE,
-  SCHEDULE_LABEL,
-  ACTIVITIES_LABEL,
-  SCHEDULE_ICON,
-  ADMIN_SCHEDULE_ICON,
-  ACTIVITIES_ICON,
-  ADMIN_ACTIVITIES_ICON,
-} from "./composables/constants";
-import { onMounted, ref } from "vue";
+import { ref } from "vue";
+import { Toast, ConfirmDialog } from "primevue";
 import { useGlobalStore } from "./stores/GlobalStore";
-import { useAuth } from "./composables/useAuth";
+import BottomNav from "./components/BottomNav.vue";
+import MoreSheet from "./components/MoreSheet.vue";
 
-const router = useRouter();
 const { setData } = useGlobalStore();
-const { user, signInWithGoogle } = useAuth();
+setData();
 
-const handleAdminNav = async (route: string) => {
-  if (!user.value) {
-    await signInWithGoogle();
-  }
-  if (user.value) {
-    router.push(route);
-  }
-};
-
-const items = ref([
-  {
-    label: ANNOUNCEMENTS_LABEL,
-    icon: ANNOUNCEMENTS_ICON,
-    command: () => router.push(ANNOUNCEMENTS_ROUTE),
-  },
-  {
-    label: GROUPS_LABEL,
-    icon: GROUPS_ICON,
-    command: () => router.push(GROUPS_ROUTE),
-  },
-  {
-    label: SCHEDULE_LABEL,
-    icon: SCHEDULE_ICON,
-    items: [
-      {
-        label: DAY_SCHEDULE_LABEL,
-        icon: DAY_SCHEDULE_ICON,
-        command: () => router.push(DAY_SCHEDULE_ROUTE),
-      },
-      {
-        label: EVENING_SCHEDULE_LABEL,
-        icon: EVENING_SCHEDULE_ICON,
-        command: () => router.push(EVENING_SCHEDULE_ROUTE),
-      },
-    ],
-  },
-  {
-    label: ACTIVITIES_LABEL,
-    icon: ACTIVITIES_ICON,
-    items: [
-      {
-        label: CLUBS_LABEL,
-        icon: CLUBS_ICON,
-        command: () => router.push(CLUBS_ROUTE),
-      },
-      {
-        label: EVENTS_LABEL,
-        icon: EVENTS_ICON,
-        command: () => router.push(EVENTS_ROUTE),
-      },
-      {
-        label: GOLDEN_VERSES_LABEL,
-        icon: GOLDEN_VERSES_ICON,
-        command: () => router.push(GOLDEN_VERSES_ROUTE),
-      },
-    ],
-  },
-  {
-    label: ADMIN_LABEL,
-    icon: ADMIN_ICON,
-    items: [
-      {
-        label: ADMIN_ANNOUNCEMENTS_LABEL,
-        icon: ANNOUNCEMENTS_ICON,
-        command: () => handleAdminNav(ADMIN_ANNOUNCEMENTS_ROUTE),
-      },
-      {
-        label: GROUPS_LABEL,
-        icon: GROUPS_ICON,
-        command: () => handleAdminNav(ADMIN_GROUPS_ROUTE),
-      },
-      {
-        label: SCHEDULE_LABEL,
-        icon: ADMIN_SCHEDULE_ICON,
-        items: [
-          {
-            label: ADMIN_DAY_SCHEDULE_LABEL,
-            icon: DAY_SCHEDULE_ICON,
-            command: () => handleAdminNav(ADMIN_DAY_SCHEDULE_ROUTE),
-          },
-          {
-            label: ADMIN_EVENING_SCHEDULE_LABEL,
-            icon: EVENING_SCHEDULE_ICON,
-            command: () => handleAdminNav(ADMIN_EVENING_SCHEDULE_ROUTE),
-          },
-        ],
-      },
-      {
-        label: ACTIVITIES_LABEL,
-        icon: ADMIN_ACTIVITIES_ICON,
-        items: [
-          {
-            label: ADMIN_CLUBS_LABEL,
-            icon: CLUBS_ICON,
-            command: () => handleAdminNav(ADMIN_CLUBS_ROUTE),
-          },
-          {
-            label: ADMIN_CLUB_BOOKINGS_LABEL,
-            icon: CLUB_BOOKINGS_ICON,
-            command: () => handleAdminNav(ADMIN_CLUB_BOOKINGS_ROUTE),
-          },
-          {
-            label: ADMIN_EVENTS_LABEL,
-            icon: EVENTS_ICON,
-            command: () => handleAdminNav(ADMIN_EVENTS_ROUTE),
-          },
-          {
-            label: GOLDEN_VERSES_LABEL,
-            icon: GOLDEN_VERSES_ICON,
-            command: () => handleAdminNav(ADMIN_GOLDEN_VERSES_ROUTE),
-          },
-        ],
-      },
-    ],
-  },
-]);
-
-onMounted(() => {
-  setData();
-});
+const showMore = ref(false);
 </script>
 
 <template>
   <div
-    class="font-bpg flex min-h-svh w-full flex-col items-center justify-start bg-[black] p-4 lg:p-10 text-white"
+    class="font-bpg flex min-h-svh w-full flex-col items-center justify-start bg-[#03060f] text-white"
   >
-    <div class="flex gap-3 justify-center items-center mb-6">
-      <h1 class="text-3xl font-semibold tracking-tight">ბანაკი "ვაზი"</h1>
-      <img src="./assets/images/logo.png" class="w-12 h-12 object-contain" />
-    </div>
+    <div class="w-full pb-20 lg:max-w-2xl">
+      <div class="flex items-center justify-center gap-3 px-4 pt-8 pb-4">
+        <h1 class="text-2xl font-semibold tracking-tight">ბანაკი "ვაზი"</h1>
+        <img src="./assets/images/logo.png" class="h-10 w-10 object-contain" />
+      </div>
 
-    <div
-      class="z-20 w-full rounded-xl bg-[#18181B] p-2 lg:min-w-225 lg:p-4 xl:max-w-300 shadow-2xl"
-    >
-      <div>
-        <Menubar
-          :model="items"
-          class="mb-4 rounded-none! border-t-0! border-r-0! border-b! border-l-0! border-surface-700!"
-        />
-
-        <div class="p-2 min-h-100">
-          <router-view v-slot="{ Component }">
-            <transition name="fade" mode="out-in">
-              <component :is="Component" />
-            </transition>
-          </router-view>
-        </div>
+      <div class="px-4">
+        <router-view v-slot="{ Component }">
+          <transition name="fade" mode="out-in">
+            <component :is="Component" />
+          </transition>
+        </router-view>
       </div>
     </div>
+
+    <BottomNav @open-more="showMore = true" />
+    <MoreSheet :visible="showMore" @close="showMore = false" />
 
     <Toast
       class="right-0 left-0 mx-auto w-full max-w-[100vw] px-2 [&_.p-toast-message]:max-w-full [&_.p-toast-message]:rounded-xl [&_.p-toast-message]:wrap-break-word [&_.p-toast-message]:whitespace-normal"
